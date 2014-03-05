754f19dbe1923a923cd918956d98956f7b45dfe6
jianping liu <rabbit.ljp@gmail.com>
2014-03-05 22:44:16 +0800

    mod param *

--- main.go
+++ main.go
@@ -7,22 +7,22 @@ type Echo struct{
 	wechat.Callback
 }
 
-func (e *Echo) MsgText(txt entry.TextRequest, back chan interface{}){
+func (e *Echo) MsgText(txt *entry.TextRequest, back chan interface{}){
 	wechat.Info("Echo: MsgText ", txt)
 }
-func (e *Echo) MsgImage(img entry.ImageRequest, back chan interface{}){
+func (e *Echo) MsgImage(img *entry.ImageRequest, back chan interface{}){
 	wechat.Info("Echo: MsgImage ", img)
 }
-func (e *Echo) MsgVoice(voice entry.VoiceRequest, back chan interface{}){
+func (e *Echo) MsgVoice(voice *entry.VoiceRequest, back chan interface{}){
 	wechat.Info("Echo: MsgVoice ", voice)
 }
-func (e *Echo) MsgVideo(video entry.VideoRequest, back chan interface{}){
+func (e *Echo) MsgVideo(video *entry.VideoRequest, back chan interface{}){
 	wechat.Info("Echo: MsgVideo ", video)
 }
-func (e *Echo) MsgLink(link entry.LinkRequest, back chan interface{}){
+func (e *Echo) MsgLink(link *entry.LinkRequest, back chan interface{}){
 	wechat.Info("Echo: MsgLink ", link)
 }
-func (e *Echo) Location(location entry.LocationRequest, back chan interface{}){
+func (e *Echo) Location(location *entry.LocationRequest, back chan interface{}){
 	wechat.Info("Echo: Location ", location)
 }
 func (e *Echo) EventSubscribe(oid string, back chan interface{}){
